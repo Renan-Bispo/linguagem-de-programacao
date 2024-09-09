c1451e0e88958f3348c7dbd9cf35dd9d41f74b06
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-09 16:03:47 -0300

    refectoring

--- main/Estruturas-de-repeticao.py
+++ main/Estruturas-de-repeticao.py
@@ -1,29 +1,29 @@
-# numeros = [1, 2, 3, 4, 5]
-#
-# for num in numeros:
-#     print(num)
+numeros = [1, 2, 3, 4, 5]
+
+for num in numeros:
+    print(num)
 from unicodedata import numeric
 
-# numero = int(input("Digite um número ou digite 0 para sair: "))
-# while numero != 0:
-#     if numero % 2 == 0:
-#         print('O número é par')
-#     else:
-#         print('O numero é ímpar')
-#     numero = int(input("Digite um número ou digite 0 para sair: "))
-#
-# for y in range(2, 7):
-#     print(y)
+numero = int(input("Digite um número ou digite 0 para sair: "))
+while numero != 0:
+     if numero % 2 == 0:
+         print('O número é par')
+     else:
+         print('O numero é ímpar')
+     numero = int(input("Digite um número ou digite 0 para sair: "))
 
-# for number in range(1, 11):
-#     if number % 2 == 0:
-#         print(f'O primeiro numero par : {number}')
-#         break
+for y in range(2, 7):
+    print(y)
 
-# for numero in range(1, 11):
-#     if numero == 5:
-#         continue
-#     print(numero)
+for number in range(1, 11):
+     if number % 2 == 0:
+        print(f'O primeiro numero par : {number}')
+        break
+
+for numero in range(1, 11):
+     if numero == 5:
+         continue
+     print(numero)
 
 
 filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4', 'Filme 5']
