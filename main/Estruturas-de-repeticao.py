36650eee4753fb69549baa1e7295c0e8e8d8a073
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-09 15:10:13 -0300

    estruturas de repetição for e while; aprendendo sobre range na estrututura de repetição

---
+++ main/Estruturas-de-repeticao.py
@@ -0,0 +1,15 @@
+# numeros = [1, 2, 3, 4, 5]
+#
+# for num in numeros:
+#     print(num)
+
+# numero = int(input("Digite um número ou digite 0 para sair: "))
+# while numero != 0:
+#     if numero % 2 == 0:
+#         print('O número é par')
+#     else:
+#         print('O numero é ímpar')
+#     numero = int(input("Digite um número ou digite 0 para sair: "))
+#
+for y in range(2, 7):
+    print(y)
