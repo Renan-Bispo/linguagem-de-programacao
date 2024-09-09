eea785553a51f55809a6b3d3cf2c21700f0fa2a3
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-09 15:32:16 -0300

    concluindo atividade de criar um loop para avaliação de filmes

--- main/Estruturas-de-repeticao.py
+++ main/Estruturas-de-repeticao.py
@@ -20,7 +20,29 @@ from unicodedata import numeric
 #         print(f'O primeiro numero par : {number}')
 #         break
 
-for numero in range(1, 11):
-    if numero == 5:
-        continue
-    print(numero)
+# for numero in range(1, 11):
+#     if numero == 5:
+#         continue
+#     print(numero)
+
+
+filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4', 'Filme 5']
+
+print("Bem-vindo a avaliação de filmes")
+print("Digite 0 para sair a qualquer momento.")
+
+for filme in filmes:
+    classificacao = input(f"Dê uma nota de 1 a 5 ao filme : {filme} ou digite 0 para sair.")
+
+    if classificacao == 0:
+        print("Uma pena que não deseja avaliar nossos filmes :(")
+        break
+
+    classificacao = int(classificacao)
+
+    if classificacao < 1 or classificacao > 5:
+        print("Digite uma avaliação válida (De 1 a 5)")
+        break
+    else:
+        print(f"Você classificou o filme {filme} com {classificacao} estrelas")
+
