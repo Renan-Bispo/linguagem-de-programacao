8fc87cc5c7af70f067346f6780d1a03247abd9de
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-11 07:51:51 -0300

    operações que podem ser aplicadas num sequencia

---
+++ main/Estrutura_de_dados.py
@@ -0,0 +1,9 @@
+texto = "Explorando a diversidade das linguagens de programação com Python"
+#operações que podem ser aplicadas a sequências.
+print(f"Tamanho do texto: {len(texto)}")
+print(f"Palavra Python presente no texto: {'Python' in texto}")
+print(f"Quantidade de E no texto: {texto.count('e')}")
+print(f"As primeiras cinco letras do texto: {texto[:5]}")
+
+
+
