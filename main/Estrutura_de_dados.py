8aa2f417bb94eb7e394b658d2f2feb1be8b9e817
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-11 08:16:13 -0300

    explorando listas e sua mutabilidade

--- main/Estrutura_de_dados.py
+++ main/Estrutura_de_dados.py
@@ -6,4 +6,17 @@ print(f"Quantidade de E no texto: {texto.count('e')}")
 print(f"As primeiras cinco letras do texto: {texto[:5]}")
 
 
+#trabalhando com listas:
+cores = ['Vermelho', 'Azul', 'Verde', 'Amarelo', 'Roxo']
 
+for cor in cores:
+    print(f"Posição = {cores.index(cor)}, cor = {cor}")
+
+
+#as list comprehensions
+linguagens = ['Python', 'Java', 'JavaScript', 'C', 'C#', 'C++','Swift', 'Go', 'Kotlin']
+print("Antes de aplicar o comprehencions = ", linguagens)
+
+linguagens = [item.lower() for item in linguagens]
+
+print("Depois de aplicar o comprehencions = ", linguagens)
