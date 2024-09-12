39a99730d780c6de071fcd5e300eea1cbfe19193
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-12 09:26:59 -0300

    atividade pratica com classes, objetos, metodos, heranças e polimorfimos com veiculos

--- main/Classes_e_Metodos.py
+++ main/Classes_e_Metodos.py
@@ -37,3 +37,34 @@ print(f"{rex.name} faz {rex.fazer_barulho()}")
 xaina = Gato("Xaina")
 print(f"{xaina.name} faz {xaina.fazer_barulho()}")
 
+#Atividade pratica
+class Veiculo:
+    def __init__(self, mark, model,year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+        self.speed = 0
+
+    def accelerate(self, increment):
+        self.speed += increment
+
+    def brake(self, decrement):
+        self.speed -= decrement
+
+    def status(self):
+        return f"Marca: {self.mark}, Modelo: {self.model}, Ano: {self.year}, Velocidade: {self.speed} km/h"
+
+
+class Car(Veiculo):
+    def __init__(self, mark, model, year, power):
+        super().__init__(mark, model, year)
+        self.power = power
+
+    def accelerate(self, increment):
+        self.speed += increment + self.power
+
+carro1 = Car("Toyta", "Supra Mk4", 2008, 130)
+
+carro1.accelerate(80)
+
+print(carro1.status())
