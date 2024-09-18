17721f85f4d51984c60f88586e6d874390393011
Renan Bispo <renanbispouniversity@gmail.com>
2024-09-18 10:47:54 -0300

    atividade intensiva para praticar o uso de classes, metodos e libs em python

---
+++ main/atividade_pratica2.py
@@ -0,0 +1,42 @@
+class Livro:
+    def __init__(self, titulo, autor, genero):
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = genero
+
+
+    def __repr__(self):
+        return f"{self.titulo} por {self.autor} ({self.genero})"
+
+def add_livro(lista_livro, titulo, autor, genero):
+    novo_livro = Livro(titulo, autor, genero)
+    lista_livro.append(novo_livro)
+
+def remove_livro(lista_livro, titulo):
+    for livro in lista_livro:
+        if livro.titulo == titulo:
+            lista_livro.remove(livro)
+            print(f"Livro {livro.titulo} foi removido")
+
+
+
+def listar_livros(lista_livro):
+    if not lista_livro:
+        print("Nenhum livro encontrado")
+    else:
+        print("Lista de livros:")
+        for livro in lista_livro:
+            print(livro)
+
+
+livros = []
+
+add_livro(livros,"Até que nada mais importe", "Luciano Subirá", "Religioso")
+add_livro(livros,"O sermão do monte", "Aquele cara", "Religioso")
+add_livro(livros,"O poder da autorresponsaiblidade", "Quem escreveu", "auto-ajuda")
+
+listar_livros(livros)
+remove_livro(livros,"O sermão do monte")
+
+
+listar_livros(livros)
